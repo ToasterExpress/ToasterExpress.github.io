a5ad55f8a5a26bf15cbc6b60b2a68e9f99b1ed36
Lucy <lucisisneros@gmail.com>
2023-09-03 13:41:40 -0600

    Created transition effect Created slideshow function to change image on a timer.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
 
-  let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -22,14 +21,32 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n >= slides.length) { slideIndex = 0 }
+  if (n < 0) { slideIndex = slides.length - 1 }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+}
+
+let index = 0;
+displaySlides();
+function displaySlides() {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  if (slideIndex >= slides.length) {
+    console.log("reset");
+    slideIndex = 0;
+  }
+  slides[slideIndex].style.display = "block";
+  showSlides(slideIndex);
+  slideIndex++;
+  setTimeout(displaySlides, 5000);
 }
